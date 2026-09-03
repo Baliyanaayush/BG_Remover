a4a272761e13d8bd05426d191583cc1d1dec6fd8
Aayush Baliyan <aayushkumar93036@gmail.com>
2026-09-03 23:05:40 +0530

    Fix user webhook route

--- Backend/src/routes/userroutes.js
+++ Backend/src/routes/userroutes.js
@@ -1,13 +1,12 @@
 const express = require("express");
 const {
   clerkWebhooks,
-  userCredits,
 } = require("../controllers/userController");
 
 const userRouter = express.Router();
 
 userRouter.post("/webhooks", clerkWebhooks);
 
-userRouter.post("/credits", userCredits);
+// userRouter.post("/credits", userCredits);
 
 module.exports = userRouter;
