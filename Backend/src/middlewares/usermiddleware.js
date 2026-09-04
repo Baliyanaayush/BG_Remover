4f1cb14153c5b4c8d2a670f8268e498f5e2bdaa6
Aayush Baliyan <aayushkumar93036@gmail.com>
2026-09-04 12:34:27 +0530

    testing2

--- Backend/src/middlewares/usermiddleware.js
+++ Backend/src/middlewares/usermiddleware.js
@@ -1,25 +1,40 @@
-const jwt = require("jsonwebtoken")
 
-const UserMiddleware = async(req, res)=>{
-   try {
-     const {token} = req.headers
-     if(!token){
-        res.json({
-            success:false,
-            message:("Not Authorised login again")
-        })
-     }
-     const tokenDecoded = jwt.decode(token)
-     req.body.clerkId = tokenDecoded.clerkId
+const jwt = require("jsonwebtoken");
 
-     next();
+const UserMiddleware = async (req, res, next) => {
+  try {
+    const { token } = req.headers;
 
-   } catch (error) {
-    res.json({
-        success:false,
-        message:error.message
-    })
-   }
-}
+    if (!token) {
+      return res.status(401).json({
+        success: false,
+        message: "Not Authorised, login again",
+      });
+    }
 
-module.exports = UserMiddleware
+    const tokenDecoded = jwt.verify(
+      token,
+      process.env.JWT_SECRET
+    );
+
+    if (!tokenDecoded?.clerkId) {
+      return res.status(401).json({
+        success: false,
+        message: "Invalid token",
+      });
+    }
+
+    req.body.clerkId = tokenDecoded.clerkId;
+
+    next();
+
+  } catch (error) {
+    return res.status(401).json({
+      success: false,
+      message: error.message,
+    });
+  }
+};
+
+module.exports = UserMiddleware;
+
